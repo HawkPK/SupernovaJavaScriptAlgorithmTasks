a8f9be1efc1c5371b4437952386dc646a9f7149f
Hawk84 <paweljastrzabek@o2.pl>
2017-05-17 00:51:32 +0200

    #4 - final solution for sum positive number task

---
+++ 8_kyu/sumPositive.js
@@ -0,0 +1,26 @@
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: array may be empty, in this case return 0.
+*/
+
+function sumPositiveNumbers(arr){
+
+if(arr.length === 0){
+    return 0;
+}
+
+    var result = 0;
+    
+for(var i = 0; i < arr.length; i++){
+
+    if(arr[i]>0){
+        result = result + arr[i];
+    }
+}
+    return result;
+}
+
+console.log(sumPositiveNumbers([1,-4,7,12]));
